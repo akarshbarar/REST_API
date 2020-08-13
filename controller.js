831b24491a8c3bfb6cf97de2817564f89b075481
Akarsh Barar <barar.akarsh8@gmail.com>
2020-08-13 20:54:04 +0530

    FIrst Commit

---
+++ controller.js
@@ -0,0 +1,37 @@
+const url = require('url');
+const users = require('./userData.js');
+exports.getUsers = function(req, res) {
+  const reqUrl = url.parse(req.url, true)  
+  var response = [
+    {
+      "message": "Here are the list of users "
+    },
+    users
+  ];  
+  res.statusCode = 200;
+  res.setHeader('content-Type', 'Application/json');
+  res.end(JSON.stringify(response))
+ }
+
+ exports.createUser = function(req, res) {
+    body = '';
+    
+    req.on('data',  function (chunk) {
+      body += chunk;
+    });
+    
+    req.on('end', function () {
+      postBody = JSON.parse(body);
+      users.push(postBody);
+      var response = [
+        {
+          "text": "User added successfully"
+        },
+        postBody
+      ]
+      
+      res.statusCode = 201;
+      res.setHeader('content-Type', 'Application/json');
+      res.end(JSON.stringify(response))
+    })
+  }
